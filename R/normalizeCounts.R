f632db187bd749e1fc7278740ff1b2b4b7e6969a
Juan Ramon Gonzalez Ruiz <jrgonzalez@creal.cat>
2012-03-19 10:24:26 +0000

    Imported normalization method from cqn package
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/tweeDEseq@64089 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalizeCounts.R
+++ R/normalizeCounts.R
@@ -2,34 +2,82 @@
 ## taken by the function exactTest() from the edgeR package
 ## right before differential expression is tested within that function
 
-normalizeCounts <- function(counts, group, common.disp=FALSE, prior.n=1) {
-  message("Using edgeR normalization methods.")
-  d <- DGEList(counts=counts, group=group)
-  message("Calculating normalization factors with the TMM method.")
-  d <- calcNormFactors(d)
-  message("Estimating common dispersion.")
-  d <- estimateCommonDisp(d)
-  if (!common.disp) {
-    message("Estimating tagwise dispersions.")
-    d <- estimateTagwiseDisp(d, prior.n=prior.n)
+normalizeCounts <- function(counts, group=rep.int(1,ncol(counts)), method=c("TMM", "cqn"),
+                            common.disp=FALSE, prior.n=1, annot=NULL, lib.sizes=NULL, verbose=TRUE) {
+  method <- match.arg(method)
+
+  if (is.null(lib.sizes))
+    lib.sizes <- colSums(counts)
+
+  if (method == "TMM") {
+    if (verbose)
+      message("Using edgeR-TMM normalization.")
+    d <- edgeR::DGEList(counts=counts, group=group, lib.size=lib.sizes)
+    if (verbose)
+      message("Calculating normalization factors with the TMM method.")
+    d <- edgeR::calcNormFactors(d)
+    if (verbose)
+      message("Estimating common dispersion.")
+    d <- edgeR::estimateCommonDisp(d)
+    if (!common.disp) {
+      if (verbose)
+        message("Estimating tagwise dispersions.")
+      d <- edgeR::estimateTagwiseDisp(d, prior.n=prior.n)
+    }
+    if (verbose)
+      message("Calculating effective library sizes.")
+    lib.size <- d$samples$lib.size * d$samples$norm.factors
+    d2 <- edgeR::DGEList(counts=counts, group=group, lib.size=lib.size)
+    if (common.disp) {
+      dispersion <- rep(d$common.dispersion, nrow(counts))
+      if (verbose)
+        message("Adjusting counts to effective library sizes using a common dispersion.")
+    } else {
+      dispersion <- d$tagwise.dispersion
+      if (verbose)
+        message("Adjusting counts to effective library sizes using tagwise dispersions.")
+    }
+    dispersion <- pmax(dispersion, 1e-06)
+    counts <- ceiling(edgeR::equalizeLibSizes(d2, disp=dispersion, null.hypothesis=TRUE)$pseudo-0.5)
   }
-  message("Calculating effective library sizes.")
-  lib.size <- d$samples$lib.size * d$samples$norm.factors
-  d2 <- DGEList(counts=counts, group=group, lib.size=lib.size)
-  if (common.disp) {
-    dispersion <- rep(d$common.dispersion, nrow(counts))
-    message("Adjusting counts to effective library sizes using a common dispersion.")
-  } else {
-    dispersion <- d$tagwise.dispersion
-    message("Adjusting counts to effective library sizes using tagwise dispersions.")
+
+  if (method == "cqn") {
+    if (verbose)
+      message("Using cqn normalization.")
+    if (!is.matrix(annot) && !is.data.frame(annot) || is.null(dim(annot)) || dim(annot)[2] != 2)
+      stop("cqn requires a two-column matrix or data frame in the argument 'annot'. Do help(normalizeCounts) for further information.")
+    common <- intersect(rownames(counts), rownames(annot[!is.na(annot[, 1]) & !is.na(annot[, 2]), ]))
+    if (length(common) == 0)
+      stop("No row names in 'counts' match any row name in 'annot'.")
+
+    cqnNorm <- cqn::cqn(counts[common, ],
+                        lengths=as.numeric(annot[common, 1]),
+                        x=as.numeric(annot[common, 2]),
+                        sizeFactors=lib.sizes, verbose=verbose)
+    log2rpm <- cqnNorm$y + cqnNorm$offset
+    log2r <- sweep(cqnNorm$y + cqnNorm$offset, 2, log2(lib.sizes/1e6), FUN="+")
+    counts <- ceiling(2^log2r-0.5)
   }
-  dispersion <- pmax(dispersion, 1e-06)
-  counts <- ceiling(equalizeLibSizes(d2, disp=dispersion, null.hypothesis=TRUE)$pseudo-0.5)
+
   counts
 }
 
-filterCounts <- function(counts, cpm.cutoff=0.5, n.samples.cutoff=2) {
-  cpm <- 1e06 * t( t(counts) / colSums(counts))
-  sel <- rowSums(cpm > cpm.cutoff) >= n.samples.cutoff
+filterCounts <- function(counts, cpm.cutoff=0.5, n.samples.cutoff=2, mean.cpm.cutoff=0, lib.sizes=NULL) {
+  if (is.null(lib.sizes))
+    lib.sizes <- colSums(counts)
+
+  sel <- rep(TRUE, nrow(counts))
+  if (mean.cpm.cutoff <= 0) {
+    cpm <- 1e06 * t( t(counts) / lib.sizes)
+    f <- 1
+    if (n.samples.cutoff > 0 && n.samples.cutoff < 1)
+      f <- ncol(counts)
+    sel <- rowSums(cpm > cpm.cutoff) >= floor(n.samples.cutoff * f)
+  } else {
+    cpm <- sweep(log2(counts+1), 2, log2(lib.sizes/10^6))
+    mean.cpm <- rowMeans(cpm)
+    sel <- mean.cpm > log2(mean.cpm.cutoff)
+  }
+
   counts[sel, ]
 }
